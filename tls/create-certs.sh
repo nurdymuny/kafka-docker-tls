c550b742e8e087c24c397a252e09f12fb6cc0546
Bee Davis <bee_davis@alumni.brown.edu>
2024-12-23 20:58:59 -0800

    fixed ... again

--- tls/create-certs.sh
+++ tls/create-certs.sh
@@ -4,36 +4,41 @@ set -o nounset \
     -o verbose \
     -o xtrace
 
-# Variables
+###############################################################################
+# Variables (update as needed)
+###############################################################################
 CA_PASSPHRASE="H62Rh4zA4opsOdsHylyENchmIqAldPCTa1c"
 KEYSTORE_PASSPHRASE="cjIgiPUKEyIA66bu4Hz4W1YnrZaXCskwp9F"
-KEY_PASS="ww6r0IaHBb945b9BB1oKYP6hDr5IhFpYAhz"
 ORGANIZATION_NAME="NASA"
 ORGANIZATION_UNIT="Luna"
 LOCATION="Houston"
 STATE="TX"
 COUNTRY="US"
-DAYS_VALID=3650  # 10 years
-KEY_SIZE=2048    # RSA key size (could go 4096 if desired)
+DAYS_VALID=3650   # 10 years
+KEY_SIZE=2048     # RSA key size (could go 4096 if desired)
 
-# Common name for your CA certificate
+# Common Name for the CA
 CA_COMMON_NAME="ca.luna.nasa.gov"
 
-# The list of entities (e.g., brokers, clients) for which we'll generate keystores
+# Entities for which we generate keystores (e.g., brokers, producer, consumer)
 ENTITIES=("broker1" "broker2" "broker3" "producer" "consumer")
 
-# Create a CA key and certificate
+###############################################################################
+# 1) Create a CA key and certificate (self-signed)
+###############################################################################
 openssl req \
   -new \
   -x509 \
   -days "${DAYS_VALID}" \
-  -subj "/CN=${CA_COMMON_NAME}/OU=${ORGANIZATION_UNIT}/O=${ORGANIZATION_NAME}/L=${LOCATION}/S=${STATE}/C=${COUNTRY}" \
+  -subj "/CN=${CA_COMMON_NAME}/OU=${ORGANIZATION_UNIT}/O=${ORGANIZATION_NAME}/L=${LOCATION}/ST=${STATE}/C=${COUNTRY}" \
   -keyout lunaca.key \
   -out lunaca.crt \
   -passin  pass:"${CA_PASSPHRASE}" \
   -passout pass:"${CA_PASSPHRASE}"
 
-# Kafkacat client key/cert
+###############################################################################
+# 2) (Optional) Generate Kafkacat client key/cert
+###############################################################################
 openssl genrsa \
   -des3 \
   -passout "pass:${KEYSTORE_PASSPHRASE}" \
@@ -46,7 +51,7 @@ openssl req \
   -key kafkacat.client.key \
   -new \
   -out kafkacat.client.req \
-  -subj "/CN=kafkacat.luna.nasa.gov/OU=${ORGANIZATION_UNIT}/O=${ORGANIZATION_NAME}/L=${LOCATION}/S=${STATE}/C=${COUNTRY}"
+  -subj "/CN=kafkacat.luna.nasa.gov/OU=${ORGANIZATION_UNIT}/O=${ORGANIZATION_NAME}/L=${LOCATION}/ST=${STATE}/C=${COUNTRY}"
 
 openssl x509 \
   -req \
@@ -58,30 +63,32 @@ openssl x509 \
   -CAcreateserial \
   -passin "pass:${CA_PASSPHRASE}"
 
-
-# Generate keystores and truststores for each entity (e.g., brokers, producer, consumer)
+###############################################################################
+# 3) Generate keystores and truststores for each entity
+###############################################################################
 for ENTITY in "${ENTITIES[@]}"; do
   echo "Creating keystore and truststore for: ${ENTITY}"
 
-  # 1) Generate a keypair in the keystore
+  # a) Generate a keypair in the keystore (JKS)
   keytool -genkey -noprompt \
     -alias "${ENTITY}" \
-    -dname "CN=${ENTITY}.luna.nasa.gov, OU=${ORGANIZATION_UNIT}, O=${ORGANIZATION_NAME}, L=${LOCATION}, S=${STATE}, C=${COUNTRY}" \
+    -dname "CN=${ENTITY}.luna.nasa.gov, OU=${ORGANIZATION_UNIT}, O=${ORGANIZATION_NAME}, L=${LOCATION}, ST=${STATE}, C=${COUNTRY}" \
     -keystore "kafka.${ENTITY}.keystore.jks" \
+    -storetype JKS \
     -keyalg RSA \
     -keysize "${KEY_SIZE}" \
     -storepass "${KEYSTORE_PASSPHRASE}" \
-    -keypass "${KEY_PASS}"
+    -keypass "${KEYSTORE_PASSPHRASE}"
 
-  # 2) Create a CSR (Certificate Signing Request)
+  # b) Create a CSR
   keytool -keystore "kafka.${ENTITY}.keystore.jks" \
     -alias "${ENTITY}" \
     -certreq \
     -file "${ENTITY}.csr" \
     -storepass "${KEYSTORE_PASSPHRASE}" \
-    -keypass "${KEY_PASS}"
+    -keypass "${KEYSTORE_PASSPHRASE}"
 
-  # 3) Sign the CSR with our CA
+  # c) Sign the CSR with our CA
   openssl x509 \
     -req \
     -CA lunaca.crt \
@@ -92,35 +99,38 @@ for ENTITY in "${ENTITIES[@]}"; do
     -CAcreateserial \
     -passin pass:"${CA_PASSPHRASE}"
 
-  # 4) Import the CA certificate into the keystore
+  # d) Import the CA cert into the keystore
   keytool -keystore "kafka.${ENTITY}.keystore.jks" \
     -alias "CARoot" \
     -import \
     -file lunaca.crt \
     -storepass "${KEYSTORE_PASSPHRASE}" \
-    -keypass "${KEY_PASS}" \
-    -noprompt
+    -keypass "${KEYSTORE_PASSPHRASE}" \
+    -noprompt \
+    -storetype JKS
 
-  # 5) Import the signed certificate into the keystore
+  # e) Import the signed certificate into the keystore
   keytool -keystore "kafka.${ENTITY}.keystore.jks" \
     -alias "${ENTITY}" \
     -import \
     -file "${ENTITY}-ca-signed.crt" \
     -storepass "${KEYSTORE_PASSPHRASE}" \
-    -keypass "${KEY_PASS}" \
-    -noprompt
+    -keypass "${KEYSTORE_PASSPHRASE}" \
+    -noprompt \
+    -storetype JKS
 
-  # 6) Create a truststore and import the CA cert
+  # f) Create a truststore (JKS) and import the CA cert
   keytool -keystore "kafka.${ENTITY}.truststore.jks" \
     -alias "CARoot" \
     -import \
     -file lunaca.crt \
     -storepass "${KEYSTORE_PASSPHRASE}" \
-    -keypass "${KEY_PASS}" \
-    -noprompt
+    -keypass "${KEYSTORE_PASSPHRASE}" \
+    -noprompt \
+    -storetype JKS
 
-  # 7) Create credentials files for convenience
-  echo "${KEY_PASS}"     > "${ENTITY}_sslkey_creds"
+  # g) Create credentials files
+  echo "${KEYSTORE_PASSPHRASE}" > "${ENTITY}_sslkey_creds"
   echo "${KEYSTORE_PASSPHRASE}" > "${ENTITY}_keystore_creds"
   echo "${KEYSTORE_PASSPHRASE}" > "${ENTITY}_truststore_creds"
 
